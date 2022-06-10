78347a5efb953027e7d43bba7d9b1043213a9346
Jarno Overdijk <jarno.vanoverdijk@edu-kw1c.nl>
2022-06-10 11:42:50 +0200

    FrmSpelerAdd gevuld

--- Praktijk_Thema_8/Praktijk_Opdracht/View/FrmSpelerAdd.Designer.cs
+++ Praktijk_Thema_8/Praktijk_Opdracht/View/FrmSpelerAdd.Designer.cs
@@ -29,12 +29,307 @@ namespace Praktijk_Opdracht.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpGeboortedatum = new System.Windows.Forms.DateTimePicker();
+            this.cbSchool = new System.Windows.Forms.ComboBox();
+            this.txtGroep = new System.Windows.Forms.TextBox();
+            this.txtAchternaam = new System.Windows.Forms.TextBox();
+            this.txtTussenvoegsel = new System.Windows.Forms.TextBox();
+            this.txtVoornaam = new System.Windows.Forms.TextBox();
+            this.btnOpslaan = new System.Windows.Forms.Button();
+            this.btnAnnuleren = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnScheidsrechter = new System.Windows.Forms.Button();
+            this.btnSluiten = new System.Windows.Forms.Button();
+            this.btnScholen = new System.Windows.Forms.Button();
+            this.btnSpelers = new System.Windows.Forms.Button();
+            this.btnResultaten = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(61, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "School";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(61, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Groep";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(61, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Geboortedatum";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Achternaam";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tussenvoegsel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Voornaam";
+            // 
+            // dtpGeboortedatum
+            // 
+            this.dtpGeboortedatum.Location = new System.Drawing.Point(200, 184);
+            this.dtpGeboortedatum.Name = "dtpGeboortedatum";
+            this.dtpGeboortedatum.Size = new System.Drawing.Size(200, 23);
+            this.dtpGeboortedatum.TabIndex = 9;
+            // 
+            // cbSchool
+            // 
+            this.cbSchool.FormattingEnabled = true;
+            this.cbSchool.Location = new System.Drawing.Point(200, 264);
+            this.cbSchool.Name = "cbSchool";
+            this.cbSchool.Size = new System.Drawing.Size(200, 23);
+            this.cbSchool.TabIndex = 8;
+            // 
+            // txtGroep
+            // 
+            this.txtGroep.Location = new System.Drawing.Point(200, 223);
+            this.txtGroep.Name = "txtGroep";
+            this.txtGroep.Size = new System.Drawing.Size(200, 23);
+            this.txtGroep.TabIndex = 7;
+            // 
+            // txtAchternaam
+            // 
+            this.txtAchternaam.Location = new System.Drawing.Point(200, 145);
+            this.txtAchternaam.Name = "txtAchternaam";
+            this.txtAchternaam.Size = new System.Drawing.Size(200, 23);
+            this.txtAchternaam.TabIndex = 6;
+            // 
+            // txtTussenvoegsel
+            // 
+            this.txtTussenvoegsel.Location = new System.Drawing.Point(200, 107);
+            this.txtTussenvoegsel.Name = "txtTussenvoegsel";
+            this.txtTussenvoegsel.Size = new System.Drawing.Size(200, 23);
+            this.txtTussenvoegsel.TabIndex = 5;
+            // 
+            // txtVoornaam
+            // 
+            this.txtVoornaam.Location = new System.Drawing.Point(200, 68);
+            this.txtVoornaam.Name = "txtVoornaam";
+            this.txtVoornaam.Size = new System.Drawing.Size(200, 23);
+            this.txtVoornaam.TabIndex = 4;
+            // 
+            // btnOpslaan
+            // 
+            this.btnOpslaan.Location = new System.Drawing.Point(415, 404);
+            this.btnOpslaan.Name = "btnOpslaan";
+            this.btnOpslaan.Size = new System.Drawing.Size(173, 42);
+            this.btnOpslaan.TabIndex = 3;
+            this.btnOpslaan.Text = "Opslaan";
+            this.btnOpslaan.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuleren
+            // 
+            this.btnAnnuleren.Location = new System.Drawing.Point(16, 404);
+            this.btnAnnuleren.Name = "btnAnnuleren";
+            this.btnAnnuleren.Size = new System.Drawing.Size(173, 42);
+            this.btnAnnuleren.TabIndex = 1;
+            this.btnAnnuleren.Text = "Annuleren";
+            this.btnAnnuleren.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(16, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Wijzigen Spelers:";
+            // 
+            // btnScheidsrechter
+            // 
+            this.btnScheidsrechter.Location = new System.Drawing.Point(0, 223);
+            this.btnScheidsrechter.Name = "btnScheidsrechter";
+            this.btnScheidsrechter.Size = new System.Drawing.Size(200, 42);
+            this.btnScheidsrechter.TabIndex = 6;
+            this.btnScheidsrechter.Text = "Scheidsrechters";
+            this.btnScheidsrechter.UseVisualStyleBackColor = true;
+            // 
+            // btnSluiten
+            // 
+            this.btnSluiten.Location = new System.Drawing.Point(0, 417);
+            this.btnSluiten.Name = "btnSluiten";
+            this.btnSluiten.Size = new System.Drawing.Size(200, 42);
+            this.btnSluiten.TabIndex = 5;
+            this.btnSluiten.Text = "Sluiten";
+            this.btnSluiten.UseVisualStyleBackColor = true;
+            // 
+            // btnScholen
+            // 
+            this.btnScholen.Location = new System.Drawing.Point(0, 184);
+            this.btnScholen.Name = "btnScholen";
+            this.btnScholen.Size = new System.Drawing.Size(200, 42);
+            this.btnScholen.TabIndex = 4;
+            this.btnScholen.Text = "Scholen";
+            this.btnScholen.UseVisualStyleBackColor = true;
+            // 
+            // btnSpelers
+            // 
+            this.btnSpelers.Location = new System.Drawing.Point(0, 145);
+            this.btnSpelers.Name = "btnSpelers";
+            this.btnSpelers.Size = new System.Drawing.Size(200, 42);
+            this.btnSpelers.TabIndex = 3;
+            this.btnSpelers.Text = "Spelers";
+            this.btnSpelers.UseVisualStyleBackColor = true;
+            // 
+            // btnResultaten
+            // 
+            this.btnResultaten.Location = new System.Drawing.Point(0, 107);
+            this.btnResultaten.Name = "btnResultaten";
+            this.btnResultaten.Size = new System.Drawing.Size(200, 42);
+            this.btnResultaten.TabIndex = 2;
+            this.btnResultaten.Text = "Resultaten";
+            this.btnResultaten.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(0, 68);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(200, 42);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(15, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "De vlugge handjes";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.dtpGeboortedatum);
+            this.groupBox2.Controls.Add(this.cbSchool);
+            this.groupBox2.Controls.Add(this.txtGroep);
+            this.groupBox2.Controls.Add(this.txtAchternaam);
+            this.groupBox2.Controls.Add(this.txtTussenvoegsel);
+            this.groupBox2.Controls.Add(this.txtVoornaam);
+            this.groupBox2.Controls.Add(this.btnOpslaan);
+            this.groupBox2.Controls.Add(this.btnAnnuleren);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(200, -4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(600, 459);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.btnScheidsrechter);
+            this.groupBox1.Controls.Add(this.btnSluiten);
+            this.groupBox1.Controls.Add(this.btnScholen);
+            this.groupBox1.Controls.Add(this.btnSpelers);
+            this.groupBox1.Controls.Add(this.btnResultaten);
+            this.groupBox1.Controls.Add(this.btnHome);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, -4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 459);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // FrmSpelerAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmSpelerAdd";
             this.Text = "FrmSpelerAdd";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpGeboortedatum;
+        private System.Windows.Forms.ComboBox cbSchool;
+        private System.Windows.Forms.TextBox txtGroep;
+        private System.Windows.Forms.TextBox txtAchternaam;
+        private System.Windows.Forms.TextBox txtTussenvoegsel;
+        private System.Windows.Forms.TextBox txtVoornaam;
+        private System.Windows.Forms.Button btnOpslaan;
+        private System.Windows.Forms.Button btnAnnuleren;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnScheidsrechter;
+        private System.Windows.Forms.Button btnSluiten;
+        private System.Windows.Forms.Button btnScholen;
+        private System.Windows.Forms.Button btnSpelers;
+        private System.Windows.Forms.Button btnResultaten;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
